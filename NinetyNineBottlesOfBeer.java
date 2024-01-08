bf4e731eaf234c4542bca94d7a320b7cba4068ab
EldieSoft <150984579+EldieSoft@users.noreply.github.com>
2024-01-08 15:56:12 -0500

    Add files via upload

---
+++ NinetyNineBottlesOfBeer.java
@@ -0,0 +1,19 @@
+
+/**
+ * Prints a version of 99 Bottles of Beer
+ * 
+ * @author Leo Depew 
+ * @version 1.0 1/6/24
+ */
+public class NinetyNineBottlesOfBeer {
+    public static void main(String[] args){
+        for (int i = 99; i > 1; i--)
+        {
+            System.out.print(i);
+            System.out.println(" bottles of beer on the wall, ");
+            System.out.print(i);
+            System.out.println(" bottles of beer. Take one down, pass it around,");
+        }
+        System.out.println("1 bottle of beer on the wall.");
+    }
+}
